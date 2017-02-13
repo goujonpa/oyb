7208a7a35320144c01fd181945fb6bb78124beef
Paul Goujon <M424354@airfrance.fr>
2017-02-13 16:25:20 +0100

    wired everything together

--- oybweb/routes/index.js
+++ oybweb/routes/index.js
@@ -43,7 +43,11 @@ router.param('post', function(req, res, next, id){
 
 /* GET single post */
 router.get('/posts/:post', function(req, res){
-    res.json(req.post);
+    req.post.populate('comments', function(err, post){
+        if (err){return next(err);}
+
+        res.json(post);
+    });
 });
 
 /* PUT upvote a post */
@@ -52,6 +56,45 @@ router.put('/posts/:post/upvote', function(req, res, next){
         if(err){return next(err);}
 
         res.json(post);
+    });
+});
+
+/* POST a new comment LINKED TO post */
+router.post('/posts/:post/comments', function(req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+
+    comment.save(function(err, comment){
+        if(err){return next(err);}
+
+        req.post.comments.push(comment);
+        req.post.save(function(err, post){
+            if(err){return next(err);}
+
+            res.json(comment);
+        });
+    });
+});
+
+/* PARAM preload comment */
+router.param('comment', function(req, res, next, id){
+    var query = Comment.findById(id);
+
+    query.exec(function(err, comment){
+        if(err){return next(err);}
+        if(!comment) {return next(new Error('Couldn\'t find comment '));}
+
+        req.comment = comment;
+        return next();
+    });
+});
+
+/* PUT upvote a comment */
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+    req.comment.upvote(function(err, comment){
+        if (err) {return next(err);}
+
+        res.json(comment);
     });
 });
 
