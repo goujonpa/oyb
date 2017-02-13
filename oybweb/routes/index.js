02324413edf105967653f433f50d8195410fd805
Paul Goujon <M424354@airfrance.fr>
2017-02-13 17:45:07 +0100

    almost finished auth

--- oybweb/routes/index.js
+++ oybweb/routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var passport = require('passport');
+var jwt = require('express-jwt');
+
+var User = mongoose.model('User');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,8 +23,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST a new post */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function(err, post){
         if(err) {return next(err);}
@@ -51,7 +57,7 @@ router.get('/posts/:post', function(req, res){
 });
 
 /* PUT upvote a post */
-router.put('/posts/:post/upvote', function(req, res, next){
+router.put('/posts/:post/upvote', auth, function(req, res, next){
     req.post.upvote(function(err, post){
         if(err){return next(err);}
 
@@ -60,9 +66,10 @@ router.put('/posts/:post/upvote', function(req, res, next){
 });
 
 /* POST a new comment LINKED TO post */
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment){
         if(err){return next(err);}
@@ -90,7 +97,7 @@ router.param('comment', function(req, res, next, id){
 });
 
 /* PUT upvote a comment */
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next){
     req.comment.upvote(function(err, comment){
         if (err) {return next(err);}
 
@@ -114,6 +121,22 @@ router.post('/register', function(req, res, next){
 
     return res.json({token: user.generateJWT()})
   });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 
