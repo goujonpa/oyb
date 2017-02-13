5c8a4fac0a2f667172d30f24895731104d60cc9b
Paul Goujon <M424354@airfrance.fr>
2017-02-13 17:20:08 +0100

    going on with the project

--- oybweb/app.js
+++ oybweb/app.js
@@ -5,22 +5,30 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
+// MODELS
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
 mongoose.connect('mongodb://localhost/news');
 
+// AUTHENTICATION MODULE
+require('./config/passport');
+
+// ROUTES
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
+// launch app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,6 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+// AUTH module, must be put after the express.static middleware
+app.use(passport.initialize());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
