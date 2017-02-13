7c735ed80c9b808d31e45c58b3c7f69970e24b5f
Paul Goujon <M424354@airfrance.fr>
2017-02-13 14:33:49 +0100

    adding basic logic

--- oybweb/routes/index.js
+++ oybweb/routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET all posts */
+router.get('/posts', function(req, res, next) {
+    Post.find(function(err, posts){
+        if(err){return next(err);}
+        res.json(posts);
+    });
+});
+
+/* POST a new post */
+router.post('/posts', function(req, res, next) {
+    var post = new Post(req.body);
+
+    post.save(function(err, post){
+        if(err) {return next(err);}
+
+        res.json(post);
+    });
+});
+
+/* PARAM preload post object when specified as parameter */
+router.param('post', function(req, res, next, id){
+    var query = Post.findById(id);
+
+    query.exec(function(err, post){
+        if(err){return next(err);}
+        if(!post) {return next(new Error('Couldn\'t find post '));}
+
+        req.post = post;
+        return next();
+    });
+});
+
+/* GET single post */
+router.get('/posts/:post', function(req, res){
+    res.json(req.post);
+});
+
+/* PUT upvote a post */
+router.put('/posts/:post/upvote', function(req, res, next){
+    req.post.upvote(function(err, post){
+        if(err){return next(err);}
+
+        res.json(post);
+    });
+});
+
 
 module.exports = router;
